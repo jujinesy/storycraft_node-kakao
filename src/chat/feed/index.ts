c17131a0cbbf03799a805d6604101b1046626b36
jujinesy <jujinesy@gmail.com>
2021-05-15 20:03:15 +0900

    docs

--- src/chat/feed/index.ts
+++
@@ -1,8 +0,0 @@
-/*
- * Created on Wed Jan 27 2021
- *
- * Copyright (c) storycraft. Licensed under the MIT Licence.
- */
-
-export * from './chat-feed';
-export * from './feed-type';
--- src/event/index.ts
+++
@@ -1,8 +0,0 @@
-/*
- * Created on Wed Jan 27 2021
- *
- * Copyright (c) storycraft. Licensed under the MIT Licence.
- */
-
-export * from './event-context';
-export * from './typed';
