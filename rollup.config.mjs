0a3655f1dc2bf9c5c444ee000405aa4c668bfb17
jujinesy <jujinesy@gmail.com>
2021-05-15 19:04:58 +0900

    v4.0.4

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -5,57 +5,41 @@
  */
 
 import nodeResolve from '@rollup/plugin-node-resolve';
-import nodePolyfills from 'rollup-plugin-node-polyfills';
 import commonjs from '@rollup/plugin-commonjs';
-import typescript from 'rollup-plugin-typescript2';
+import typescript from '@rollup/plugin-typescript';
 
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-export default () => {
-  const plugins = [
-    commonjs({
-      include: 'node_modules/**',
-    }),
-    nodePolyfills(),
-    nodeResolve(),
-    typescript({
-      // bug fix
-      rollupCommonJSResolveHack: true,
-      tsconfigOverride: {
-        compilerOptions: {
-          module: 'ESNext',
-          declaration: false,
-          removeComments: true,
-        },
-      },
-    }),
-  ];
+const plugins = [
+  nodeResolve({
+    preferBuiltins: true,
+    browser: true
+  }),
+  commonjs(),
+  typescript({
+    module: 'ESNext',
+    declaration: false,
+    removeComments: true,
+  }),
+];
 
-  return [
+/** @type {import('rollup').InputOptions} */
+const opts = {
+  input: 'src/index.ts',
+  external: [
+    // Excluded
+    'axios',
+    'form-data',
+  ],
+  shimMissingExports: true,
+  output: [
     {
-      input: 'src/index.ts',
-      external: [
-        // Excluded
-        'axios',
-        'form-data',
-
-        // Substituted
-        'bson',
-        'hash-wasm',
-      ],
-      shimMissingExports: true,
-      output: [
-        {
-          dir: 'dist_esm',
-          format: 'esm',
-          sourcemap: true,
-          paths: {
-            'bson': 'https://unpkg.com/bson/dist/bson.browser.esm.js',
-            'hash-wasm': 'https://cdn.jsdelivr.net/npm/hash-wasm/dist/index.esm.min.js',
-            'eventemitter3': 'https://unpkg.com/eventemitter3@latest/umd/eventemitter3.min.js',
-          },
-        },
-      ],
-      plugins,
+      dir: 'dist_esm',
+      format: 'esm',
+      sourcemap: true,
+      preserveModules: true,
+      preserveModulesRoot: 'src'
     },
-  ];
+  ],
+  plugins,
 };
+
+export default opts;
